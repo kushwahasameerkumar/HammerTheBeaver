06d6a7d61ffc7029cd76ecdb382b1075d9521acb
Sameer Kumar Kushwaha <kushwahasameerkumar@gmail.com>
2020-02-14 03:32:42 +0530

    UI modified

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
+async function sound(src) {
+    this.sound = await document.createElement("audio");
+    this.sound.src = await src;
     this.sound.setAttribute("preload", "auto");
     this.sound.setAttribute("controls", "none");
     this.sound.style.display = "none";
@@ -11,6 +11,13 @@ function sound(src) {
     this.stop = function(){
         this.sound.pause();
     }
+
+    return this.sound;
+}
+
+function playSound(){
+    const s = document.getElementById("gameSound");
+    s.play();
 }
 
 function getRand(min, max){
@@ -18,19 +25,20 @@ function getRand(min, max){
     return parseInt(Math.random()*(max-min)+min);
 }
 function showBeaver(){
-    const rand = getRand(11, 14);
+    const rand = getRand(1, 10);
     this.element = document.getElementById(rand);
     this.element.src = "./img/beaver.jpeg";
     return rand;
 }
 function hideBeaver(currentBox){
     this.element = document.getElementById(currentBox);
-    this.element.src = "./img/hole.jpg";
+    this.element.src = "./img/hole.png";
     return 0;
 }
 
 var currentBox = 0;
 var score = 0;
+var done = false;
 function playGame(){
     var count=2;
     setInterval(function(){
@@ -40,15 +48,21 @@ function playGame(){
         }
         if(count==0){
             currentBox = hideBeaver(currentBox);
+            done = false;
         }
         count++;
-    }, 1000);
+    }, 800);
 }
+
 function hammer(i){
-    if(i==currentBox){
+    if(i==currentBox && done==false){
         score += 50;
         document.getElementById("score").innerHTML = score;
+        done=true;
     }
 }
 
 playGame();
+setTimeout(function(){
+    playSound();
+}, 3000);
